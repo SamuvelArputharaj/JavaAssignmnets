e325892ef01f4d6d71401fd475da60c9eb672dce
SamuvelArputharaj <152051757+SamuvelArputharaj@users.noreply.github.com>
2023-12-07 22:50:38 +0530

    Add files via upload

--- week2/day3/CreateAccount.java
+++ week2/day3/CreateAccount.java
@@ -1,11 +1,13 @@
 package week2.day3;
 
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class CreateAccount {
 
-	public static void main(String[] args) {
+	public static void main(String[] args) throws InterruptedException {
 		
 		ChromeDriver driver=new ChromeDriver();
 		driver.manage().window().maximize();
@@ -19,7 +21,25 @@ public class CreateAccount {
 		driver.findElement(By.id("accountName")).sendKeys("SJ ACCOUNT");
 		driver.findElement(By.name("description")).sendKeys("Selenium Automation Tester.");
 		
-		// DropDown Selection need to Work 
+	    WebElement s1 = driver.findElement(By.name("industryEnumId"));
+	    Select dropdown =new Select(s1);
+	    dropdown.selectByValue("IND_SOFTWARE");
+	    
+	    WebElement s2 = driver.findElement(By.name("ownershipEnumId"));
+		Select dropdown2 = new Select(s2);
+		dropdown2.selectByVisibleText("S-Corporation");
+		
+		WebElement s3 = driver.findElement(By.id("dataSourceId"));
+		Select dropdown3 =new Select(s3);
+		dropdown3.selectByIndex(4);
+		
+		WebElement s4 = driver.findElement(By.id("marketingCampaignId"));
+		Select dropdown4 = new Select(s4);
+		dropdown4.selectByValue("9000");
+		
+		WebElement s5 = driver.findElement(By.id("generalStateProvinceGeoId"));
+		Select dropdown5 = new Select(s5);
+		dropdown5.selectByVisibleText("Texas");
 		
 		driver.findElement(By.className("smallSubmit")).click();
 		driver.close();
--- week2/day3/FacebookRegistrationPage.java
+++ week2/day3/FacebookRegistrationPage.java
@@ -2,8 +2,12 @@ package week2.day3;
 
 
 
+import java.time.Duration;
+
 import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.support.ui.Select;
 
 public class FacebookRegistrationPage {
 
@@ -12,7 +16,7 @@ public class FacebookRegistrationPage {
 		driver.manage().window().maximize();
 		driver.get("https://en-gb.facebook.com/");
 		driver.findElement(By.linkText("Create new account")).click();
-		// Need to learn how to add implicit wait 
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
 		Thread.sleep(2000);
 		driver.findElement(By.name("firstname")).sendKeys("Chennai");
 		driver.findElement(By.name("lastname")).sendKeys("Madras");
@@ -20,7 +24,28 @@ public class FacebookRegistrationPage {
 		driver.findElement(By.name("reg_email_confirmation__")).sendKeys("Chennai@gmail.com");
 		driver.findElement(By.id("password_step_input")).sendKeys("NewChennai");
 		
+		WebElement s1 = driver.findElement(By.id("day"));
+		Select dropdown = new Select(s1);
+		dropdown.selectByValue("19");
+		
+		WebElement s2 = driver.findElement(By.id("month"));
+		Select dropdown2 = new Select(s2);
+		dropdown2.selectByVisibleText("Jun");
+		
+		WebElement s3 = driver.findElement(By.id("year"));
+		Select dropdown3 = new Select (s3);
+		dropdown3.selectByValue("1993");
+		
+		
+		WebElement radioButton = driver.findElement(By.xpath("//label[text()='Male']"));
+		radioButton.click();
+		
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(3000));
+		
 		// DropDown Selection & radio Button need to Work 
+		
+		
+		
         driver.close();
 	}
 
